c042aabfd4a98397b519827d1c44d6c45afcd6ce
MagdalenaGoranova <91832657+MagdalenaGoranova@users.noreply.github.com>
2022-05-05 15:04:08 +0100

    add wizard on start game

---
+++ init.js
@@ -0,0 +1,30 @@
+function gameFactory() {
+    let factory ={}; 
+    let startScreen = document.querySelector('.start-screen');
+    let playScreen = document.querySelector('.play-screen');
+    let wizardElement = createWizard(100, 200);
+
+    playScreen.appendChild(wizardElement);
+
+    Object.defineProperties(factory, {
+        startScreen: {
+            get: () => startScreen
+        },
+        playScreen: {
+            get: () => playScreen
+        },
+        wizardElement
+        
+    });
+    return factory;
+}
+function createWizard(posX, posY) {
+    let wizardElement = document.createElement('div');
+    wizardElement.classList.add('wizard'); 
+    wizardElement.style.top = posY + 'px';
+    wizardElement.style.left = posX + 'px';
+
+    return wizardElement;
+
+}
+
