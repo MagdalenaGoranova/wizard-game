171a09c04a6bd412a9f602ad9850a25b27830c65
MagdalenaGoranova <91832657+MagdalenaGoranova@users.noreply.github.com>
2022-05-09 10:52:06 +0100

    Add score points

--- gameFactory.js
+++ gameFactory.js
@@ -5,6 +5,7 @@ function gameFactory() {
 
     let startScreen = document.querySelector('.start-screen');
     let playScreen = document.querySelector('.play-screen');
+    let scoreScreen = document.querySelector('.score-screen'); 
     let wizardElement = createWizard(wizard.x, wizard.y);
     
     
@@ -14,13 +15,14 @@ function gameFactory() {
     let factory = {
         startScreen, 
         playScreen,
+        scoreScreen,
         wizardElement,
         createBug: () => {
             let bugElement = document.createElement('div');
             bugElement.classList.add('bug'); 
             bugElement.style.width = bugStats.width + 'px';
             bugElement.style.height = bugStats.height + 'px';
-            
+
             // bug position  - make position random every time not fixed 
             bugElement.style.left = playScreen.offsetWidth - bugStats.width + 'px';
             bugElement.style.top = (playScreen.offsetHeight - bugStats.height) * Math.random() + 'px';
