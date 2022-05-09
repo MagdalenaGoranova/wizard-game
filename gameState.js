31ea3a5eb1d34c753de0b63836fb85aa417a9a27
MagdalenaGoranova <91832657+MagdalenaGoranova@users.noreply.github.com>
2022-05-09 08:54:47 +0100

    Refactor gameState logic

---
+++ gameState.js
@@ -0,0 +1,11 @@
+function gameStateFactory() {
+    let state = {
+        wizard: {
+            x: 100, 
+            y: 200,
+        }
+    };
+    return function() {
+        return state;
+    };
+}
